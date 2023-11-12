89be5e97a12849658bbc023c5b7c54978e7097e6
Muzffar Shaikh <139469890+shaikhmuzffar@users.noreply.github.com>
2023-11-12 22:16:55 +0530

    Add files via upload

---
+++ Introduction to JS/task2.js
@@ -0,0 +1,40 @@
+// 1.Arithmetic Operators: Perform basic mathematical operations.
+
+let a = 5;
+let b = 2;
+
+let sum = a + b; // Addition
+let difference = a - b; // Subtraction
+let product = a * b; // Multiplication
+let quotient = a / b; // Division
+let remainder = a % b; // Modulus (remainder)
+
+// 2.Assignment Operators: Assign values to variables.
+
+let x = 10;
+x += 5; // Equivalent to x = x + 5
+
+// 3.Comparison Operators: Compare values and return a Boolean result.
+
+let isEqual = a === b; // Strict equality
+let isNotEqual = a !== b; // Strict inequality
+let isGreaterThan = a > b; // Greater than
+let isLessThan = a < b; // Less than
+
+// 4.Logical Operators: Combine or negate Boolean values.
+
+let logicalAnd = (a > 0) && (b < 10); // Logical AND
+let logicalOr = (a < 0) || (b > 0); // Logical OR
+let logicalNot = !(a === b); // Logical NOT
+
+// 5.Increment/Decrement Operators: Increase or decrease the value of a variable.
+
+let counter = 0;
+counter++; // Increment by 1
+counter--; // Decrement by 1
+
+// String Operators: Concatenate strings.
+
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName; // String concatenation
---
+++ JS Fundamentals/task4.js
@@ -0,0 +1,16 @@
+// Question 4: What is the purpose of declaring variables in JavaScript, and how do you declare them using the
+// 'let' keyword?
+
+// solution:
+
+// - In JavaScript, declaring variables serves the purpose of allocating memory to store and manipulate data within a program. 
+// - Variables allow developers to store values and reference them by a name, enabling flexibility and reusability in code.
+// - The let keyword is used to declare variables in JavaScript. 
+// - Here's a simple example:
+
+let myVariable; // Declaration without initialization
+
+myVariable = 42; // Initialization with a value
+
+// - In this example, myVariable is declared using let. 
+// - Initially, it doesn't have a value (undefined). Later, it is assigned the value 42. 
